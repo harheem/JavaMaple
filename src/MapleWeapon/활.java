6ccdc9b6f5dfdc69ca209f5cea4cdabad597963a
easyhooon <51016231+easyhooon@users.noreply.github.com>
2019-06-14 14:26:35 +0900

    스킬이름 뒤바껴있길래 수정합니다
    제곧내

--- src/MapleWeapon/활.java
+++ src/MapleWeapon/활.java
@@ -6,7 +6,7 @@ public class 활 extends Weapon {
 	public 활() {
 		this.wpName = new String[] { "워 보우", "올림푸스", "메투스" };
 		this.wpPower = new int[] { 10, 15, 20 };
-		this.skillName = new String[] { "언카운터블 애로우", "애로우 플래터" };
+		this.skillName = new String[] { "에로우 플래터", "언카운터블 에로우" };
 		this.skillMP = new int[] { 30, 90 };
 		this.skillPower = new int[] { 20, 50 };
 		wpIcon.add("../image/wp/BowMasterWeapon1.png");
