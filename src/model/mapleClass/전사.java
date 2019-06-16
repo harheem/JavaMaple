7b70ba63e0ea9fb82312730a482fe39cc1313969
easyhooon <51016231+easyhooon@users.noreply.github.com>
2019-06-17 03:33:31 +0900

    final balance
    전사 너프

--- src/model/mapleClass/전사.java
+++ src/model/mapleClass/전사.java
@@ -5,7 +5,7 @@ import model.Buffable;
 public abstract class 전사 extends Player implements Buffable {
 
     public 전사() {
-		this.m_hp = 1600;
+		this.m_hp = 1500;
 		this.m_mp = 300;
 		this.hp = this.m_hp;
 		this.mp = this.m_mp;
