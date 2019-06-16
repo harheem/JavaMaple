5b42aace7c65792e40dbb3a8cd3dcd0599f43940
easyhooon <51016231+easyhooon@users.noreply.github.com>
2019-06-16 22:59:39 +0900

    Revert "다크나이트 하향"
    This reverts commit e8d4dd677be8b4a30978c129cf2b80a4436114d4.

--- src/model/mapleClass/다크나이트.java
+++ src/model/mapleClass/다크나이트.java
@@ -1,6 +1,6 @@
-package MapleClass;
+package model.mapleClass;
 
-import MapleWeapon.창;
+import model.mapleWeapon.창;
 
 public class 다크나이트 extends 전사 {
 
@@ -39,8 +39,8 @@ public class 다크나이트 extends 전사 {
 			return;
 		else {
 			this.mp-=buffSkillMp;
-			this.m_hp = (int) (this.m_hp * 1.25);
-			this.m_mp = (int) (this.m_mp * 1.25);
+			this.m_hp = (int) (this.m_hp * 1.5);
+			this.m_mp = (int) (this.m_mp * 1.5);
 			this.buffTime = 3;
 		}
 	}
