6bde174db0c32476b6e31f666b5b19eb368362eb
harheem <49297157+harheem@users.noreply.github.com>
2019-06-16 10:45:10 +0900

    selectView error

--- src/MapleView/SelectView.java
+++ src/MapleView/SelectView.java
@@ -44,7 +44,7 @@ public class SelectView extends JFrame { // View에선 버튼의 생성과 추가 및 Locat
 			this.name[i] = new ImageIcon(Main.class.getResource("../image/charNameByNum/" + i + ".png"));
 			this.nameButton[i].setIcon(this.name[i]);
 			this.imgButton[i].setSize(500, 600); // setSize 말고 사진이 잘려보이지 않는 다른 기능
-			this.nameButton[i].setSize(180, 50);
+			this.nameButton[i].setSize(260, 100);
 			this.nameButton[i].setBorderPainted(false);
 			this.nameButton[i].setContentAreaFilled(false);
 			this.nameButton[i].setFocusPainted(false);
@@ -61,7 +61,7 @@ public class SelectView extends JFrame { // View에선 버튼의 생성과 추가 및 Locat
 
 			this.imgButton[num].setLocation(250, 110);
 			this.imgButton[num].setVisible(false); // 직업img버튼은 직업name버튼에 마우스를 올려야 보이도록 함.
-			this.nameButton[num].setLocation(1020, this.yLctn);
+			this.nameButton[num].setLocation(980, this.yLctn);
 			this.nameButton[num].addMouseListener(new MouseListener() { // View쪽 접근기능이 필요한 버튼은 View에서 기능(재)정의
 				@Override
 				public void mouseClicked(MouseEvent e) {
@@ -72,13 +72,13 @@ public class SelectView extends JFrame { // View에선 버튼의 생성과 추가 및 Locat
 
 				@Override
 				public void mouseEntered(MouseEvent e) {
-					this.imgButton[num].setVisible(true);
+					imgButton[num].setVisible(true);
 					show_msg(num);
 				}
 
 				@Override
 				public void mouseExited(MouseEvent e) {
-					this.imgButton[num].setVisible(false);
+					imgButton[num].setVisible(false);
 				}
 
 				@Override
