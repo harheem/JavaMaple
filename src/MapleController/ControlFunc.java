fda9ec26b5751d5e0ecbbdc5fed6d3205dc329f1
harheem <49297157+harheem@users.noreply.github.com>
2019-06-16 16:04:21 +0900

    종료버튼 추가, 체크뷰 수정

--- src/MapleController/ControlFunc.java
+++ src/MapleController/ControlFunc.java
@@ -13,9 +13,15 @@ public class ControlFunc { // 메인에서 Controller 객체생성해서 쓰게 메서드 변수 
 	private int playerIndex;
 	private int enemyIndex;
 	private int round;
-	
-	public ControlFunc() {this.round = 1;}
-	
+	private BattleView bv;
+	private CheckView cv;
+	private CheckView finCheck;
+
+	public ControlFunc() {
+		this.round = 1;
+	}
+
+	// 로그인뷰를 제어하는 메소드
 	public void logInFunc() {
 		// login실행
 		Login lg = new Login(new UserDataList());
@@ -28,111 +34,124 @@ public class ControlFunc { // 메인에서 Controller 객체생성해서 쓰게 메서드 변수 
 		}
 	}
 
+	// 셀렉트뷰를 제어하는 메소드
 	public void selectFunc() {
-		// if(lg.getS() = true){
 		SelectView sv = new SelectView();
 		while (true) {
 			if (sv.isVisible())
 				System.out.print("");
-			else {
+			else
 				break;
-			}
 		}
 		playerIndex = sv.getIndex();
 		p = creator(playerIndex);
 	}
-	
+
+	// 체크뷰를 제어하는 메소드
 	public void checkViewFunc() {
 		Random rand = new Random();
-		enemyIndex=rand.nextInt(8);
+		enemyIndex = rand.nextInt(8);
 		e = creator(enemyIndex);
-		CheckView cv=new CheckView(this.p, this.e);
+		CheckView cv = new CheckView(this.p, this.e);
 		while (true) {
 			if (cv.isVisible())
 				System.out.print("");
-			else {
+			else
 				break;
-			}
 		}
-		for(int i =1; i<round; i++) e.reinforce();
-		
+		for (int i = 1; i < round; i++)
+			e.reinforce();
 	}
 
+	// 플레이어가 다시하기를 선택했을 때 실행되는 메소드
+	public void reBattle() {
+		p = creator(playerIndex); // 능력치 초기화하기
+		e = creator(enemyIndex); // 능력치 초기화하기
+		for (int i = 1; i < round; i++) { // 해당 라운드 만큼 강화하기
+			p.reinforce();
+			e.reinforce();
+		}
+	}
+
+	public void threadSleep(int delay) {
+		try {
+			Thread.sleep(delay);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	// 배틀뷰를 제어하는 메소드
 	public boolean battleFunc() {
 		this.e.setEnemy(this.p);
 		this.p.setEnemy(this.e);
-		BattleView bv = new BattleView(this.p, this.e);		//인자 전달을 static값으로 보내주니까 static 메서드가 못씀.
+		bv = new BattleView(this.p, this.e);
 
-		while(true) {
-		if(p.isDead()) //졌으면
-		{
-			bv.dispose();
-			bv.threadStop();
-			CheckView temp = new CheckView(p,e,false);
-			while(temp.isVisible()) System.out.print(""); //입력 기다리기
-			if(!temp.restart()) //다시 싸울거면
-			{
-				p = creator(playerIndex); //능력치 초기화하기
-				e = creator(enemyIndex); //능력치 초기화하기
-				for(int i=1; i<round; i++) //해당 라운드만큼 다시 강화하기
-				{
-					p.reinforce();
-					e.reinforce();
+		while (true) {
+			if (p.isDead()) {// 플레이어가 죽었으면
+				closeBattleView();
+				cv = new CheckView(p, e, false); // 체크뷰에 결과를 인자로 전달하기
+				while (cv.isVisible())
+					System.out.print(""); // 입력 기다리기
+				if (!cv.restart()) {
+					reBattle();
+					return battleFunc();
+				} else return true;
+			} else if (e.isDead()) { // 플레이어가 이겼으면
+				closeBattleView();
+				cv = new CheckView(p, e, true);
+				threadSleep(3000);
+				cv.dispose();
+				if (round == 3) {
+					finCheck = new CheckView(p); // final view
+					while (finCheck.isVisible())
+						System.out.print(""); // 입력 기다리기
+					return finCheck.restart(); // 다시하거나 종료하기
+				} else {
+					round++;
+					p.reinforce(); // 강화하기
+					checkViewFunc(); // 다음 라운드 시작화면 보이기
+					return battleFunc(); // 다시호출
 				}
-				return battleFunc();
 			}
-			else return true;
-		}	
-		else if(e.isDead()) //이겼으면
-		{
-			bv.dispose();
-			bv.threadStop();
-			CheckView temp = new CheckView(p,e,true); //이겼다는 화면 보이기
-			try {
-				Thread.sleep(3000); //3초동안 멈추기
-			} catch (InterruptedException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			temp.dispose();
-			if(round==3)
-			{
-				CheckView finCheck = new CheckView(p); //final view
-				while(finCheck.isVisible()) System.out.print(""); //입력 기다리기
-				return finCheck.restart(); //다시하거나 종료하기
-			}
-			else
-			{
-				round++;
-				p.reinforce(); //강화하기
-				checkViewFunc(); //다음 라운드 시작화면 보이기
-				return battleFunc(); //다시호출
-			}
-		}
-		System.out.print(""); //이거없으면 while문이 실행안돼요..
+			System.out.print("");
 		}
 	}
-	
+
+	// 배틀뷰에 있는 쓰레드와 배틀뷰를 끄는 메소드
+	public void closeBattleView() {
+		bv.dispose();
+		bv.threadStop();
+	}
+
 	private Player creator(int i) {
 		Player x = null;
 		switch (i) {
-		case 0: x=new 팔라딘();
-		break;
-		case 1: x=new 다크나이트();
-		break;
-		case 2: x=new 보우마스터();
-		break;
-		case 3: x=new 신궁();
-		break;
-		case 4: x=new 불독();
-		break;
-		case 5: x=new 썬콜();
-		break;
-		case 6: x=new 나이트로드();
-		break;
-		case 7: x=new 섀도어();
-		break;
-		}	
+		case 0:
+			x = new 팔라딘();
+			break;
+		case 1:
+			x = new 다크나이트();
+			break;
+		case 2:
+			x = new 보우마스터();
+			break;
+		case 3:
+			x = new 신궁();
+			break;
+		case 4:
+			x = new 불독();
+			break;
+		case 5:
+			x = new 썬콜();
+			break;
+		case 6:
+			x = new 나이트로드();
+			break;
+		case 7:
+			x = new 섀도어();
+			break;
+		}
 		return x;
 	}
 }
--- src/MapleView/CheckView.java
+++ src/MapleView/CheckView.java
@@ -17,189 +17,186 @@ import MapleClass.Player;
 import MapleController.Main;
 //import MapleView.BattleView;
 
-public class CheckView extends JFrame implements ActionListener{
-    
+public class CheckView extends JFrame implements ActionListener {
+
 	PlayerButton pb;
 	EnemyButton eb;
-	Player p,e;
+	Player p, e;
 	private int stageNum;
 	private Component[] cpns;
-    private Image screenImage;
+	private Image screenImage;
 	private Image selectBackground;
 	private Graphics screenGraphic;
-	private int win; //생성자 구별하기위한 파라미터 
-	private String lose; // 생성자 구별하기위한 파라미터	
-	private boolean restart; //다시 시작할지를 결정
-	
-	JButton sb = new JButton(); //startButton 누르면 배틀뷰로 넘어감
-	JButton vb = new JButton(); // VSButton 기능없음 
-	JButton wb = new JButton(); //WinButton 기능없음
-	JButton lb = new JButton(); //LoseButton 기능없음
+	private int win; // 생성자 구별하기위한 파라미터
+	private String lose; // 생성자 구별하기위한 파라미터
+	private boolean restart; // 다시 시작할지를 결정
+
+	JButton sb = new JButton(); // startButton 누르면 배틀뷰로 넘어감
+	JButton vb = new JButton(); // VSButton 기능없음
+	JButton wb = new JButton(); // WinButton 기능없음
+	JButton lb = new JButton(); // LoseButton 기능없음
 	JButton rb = new JButton(); // RestartButton 누르면 졌던 스테이지의 배틀뷰로 다시 넘어감
-	JButton bb = new JButton(); // BackButton 누르면 셀렉트뷰로 넘어감 
-   
-	//배틀 화면 시작하기
-	public CheckView(Player p, Player e) { 
+	JButton bb = new JButton(); // BackButton 누르면 셀렉트뷰로 넘어감
+	JButton xb = new JButton(); // exitButton 누르면 종료
+
+	// 배틀 화면 시작하기
+	public CheckView(Player p, Player e) {
 		this.p = p;
 		this.e = e;
 		this.stageNum = p.getW().getWpIndex() + 1; // 무기 index값+1이 스테이지 넘버
-		defaultViewSet(); 
+		defaultViewSet();
 		this.creator();
-		cpns = new Component[] {sb,vb,pb,eb}; 
+		cpns = new Component[] { sb, vb, pb, eb };
 		this.cpnsSetLocation();
 		this.cpnsAdd();
 		this.setVisible(true);
 	}
-	
-	//이기거나 졌을 때
+
+	// 이기거나 졌을 때
 	public CheckView(Player p, Player e, boolean win) {
-		this.p =p;
-		this.e =e;
+		this.p = p;
+		this.e = e;
 		this.stageNum = p.getW().getWpIndex() + 1; // 무기 index값+1이 스테이지 넘버
 		defaultViewSet();
-		if(win) //이겼을 때
+		if (win) // 이겼을 때
 		{
 			this.winCreator();
 			this.winSetLocation();
-		}
-		else //졌을 때
+		} else // 졌을 때
 		{
 			this.loseCreator();
 			this.loseSetLocation();
 		}
-		cpns = new Component[] {wb,lb,rb,bb,pb,eb};
-		this.buttonAddActionListener(); //rb랑 bb에 리스너달기
+		cpns = new Component[] { wb, lb, rb, bb, pb, eb, xb };
+		this.buttonAddActionListener(); // rb랑 bb에 리스너달기
 		this.cpnsAdd();
 		this.setVisible(true);
-		
-		
+
 	}
-	
-	//finalView
+
+	// finalView
 	public CheckView(Player p) {
-		this.p =p;
+		this.p = p;
 		this.stageNum = p.getW().getWpIndex() + 1;// 무기 index값+1이 스테이지 넘버
 		defaultViewSet();
 		this.finalCreator();
-		cpns = new Component[] {wb,rb,bb,pb};
+		cpns = new Component[] { wb, rb, bb, pb, xb };
 		this.finalSetLocation();
 		this.cpnsAdd();
-		this.buttonAddActionListener(); 
-		this.setVisible(true);			
-	}	
-	
+		this.buttonAddActionListener();
+		this.setVisible(true);
+	}
+
 	public void creator() {
 		sb.setBorderPainted(false);
-	    sb.setContentAreaFilled(false);
-	    sb.setFocusPainted(false);
-	    sb.setSize(310, 160);
-	    sb.setIcon(new ImageIcon(Main.class.getResource("../image/check/startButton.png")));
-	    sb.addActionListener(this);
-		
+		sb.setContentAreaFilled(false);
+		sb.setFocusPainted(false);
+		sb.setSize(310, 160);
+		sb.setIcon(new ImageIcon(Main.class.getResource("../image/check/startButton.png")));
+		sb.addActionListener(this);
+
 		vb.setBorderPainted(false);
 		vb.setContentAreaFilled(false);
 		vb.setIcon(new ImageIcon(Main.class.getResource("../image/check/vsimg.png")));
 		vb.setFocusPainted(false);
 		vb.setSize(160, 110);
-		
-		this.pb = new PlayerButton(p,null);
-		
+
+		this.pb = new PlayerButton(p, null);
+
 		this.eb = new EnemyButton(e);
-		
-			
+
 	}
+
 	public void loseCreator() {
-        this.pb = new PlayerButton(p,null);
-        pb.setEnabled(false);
-		
-        this.eb = new EnemyButton(e);
-       
-        wb.setBorderPainted(false);
-		wb.setContentAreaFilled(false);
-		wb.setFocusPainted(false);
-		wb.setIcon(new ImageIcon(Main.class.getResource("../image/check/Winicon.png")));
-		wb.setSize(240, 140); 
-		
-		lb.setBorderPainted(false);
-		lb.setContentAreaFilled(false);
-		lb.setFocusPainted(false);
-		lb.setIcon(new ImageIcon(Main.class.getResource("../image/check/Loseicon.png")));
-		lb.setSize(240, 140); 
-		
-		rb.setBorderPainted(false);
-	    rb.setContentAreaFilled(false);
-	    rb.setFocusPainted(false);
-	    rb.setIcon(new ImageIcon(Main.class.getResource("../image/check/restartButton.png")));
-	    rb.setSize(310, 160);
-				
-		bb.setBorderPainted(false);
-	    bb.setContentAreaFilled(false);
-	    bb.setFocusPainted(false);
-	    bb.setIcon(new ImageIcon(Main.class.getResource("../image/check/backButton.png")));
-	    bb.setSize(310, 160);
-				
-	}
-	
-	public void winCreator() {
-	
-		sb.setBorderPainted(false);
-	    sb.setContentAreaFilled(false);
-	    sb.setFocusPainted(false);
-	    sb.setIcon(new ImageIcon(Main.class.getResource("../image/check/startButton.png")));
-	    sb.setSize(310, 160);
-	    sb.addActionListener(this);
-		this.pb = new PlayerButton(p,null);
-       
-		
-        this.eb = new EnemyButton(e);
-        eb.setEnabled(false);
-        
-        wb.setBorderPainted(false);
-		wb.setContentAreaFilled(false);
-		wb.setFocusPainted(false);
-		wb.setIcon(new ImageIcon(Main.class.getResource("../image/check/Winicon.png")));
-		wb.setSize(240, 140); 
-		
-		lb.setBorderPainted(false);
-		lb.setContentAreaFilled(false);
-		lb.setFocusPainted(false);
-		lb.setIcon(new ImageIcon(Main.class.getResource("../image/check/Loseicon.png")));
-		lb.setSize(240, 140);
-				
-	}
-	
-	public void finalCreator() {
-		
-		
-		this.pb = new PlayerButton(p,null);
-		
+		this.pb = new PlayerButton(p, null);
+		pb.setEnabled(false);
+
+		this.eb = new EnemyButton(e);
+
 		wb.setBorderPainted(false);
 		wb.setContentAreaFilled(false);
 		wb.setFocusPainted(false);
 		wb.setIcon(new ImageIcon(Main.class.getResource("../image/check/Winicon.png")));
 		wb.setSize(240, 140);
-	
+
+		lb.setBorderPainted(false);
+		lb.setContentAreaFilled(false);
+		lb.setFocusPainted(false);
+		lb.setIcon(new ImageIcon(Main.class.getResource("../image/check/Loseicon.png")));
+		lb.setSize(240, 140);
+
 		rb.setBorderPainted(false);
-	    rb.setContentAreaFilled(false);
-	    rb.setFocusPainted(false);
-	    rb.setIcon(new ImageIcon(Main.class.getResource("../image/check/restartButton.png")));
-	    rb.setSize(310, 160);
+		rb.setContentAreaFilled(false);
+		rb.setFocusPainted(false);
+		rb.setIcon(new ImageIcon(Main.class.getResource("../image/check/restartButton.png")));
+		rb.setSize(310, 160);
+
 		bb.setBorderPainted(false);
-	    bb.setContentAreaFilled(false);
-	    bb.setFocusPainted(false);
-	    bb.setIcon(new ImageIcon(Main.class.getResource("../image/check/backButton.png"))); //이것만 바꿔주면 fianlview도 완성입니다..ㅠㅠ 
-//	    bb.setIcon(new ImageIcon(Main.class.getResource("../image/check/????.png")));
-	    bb.setSize(310, 160);
-		//this.a = new JLabel();
-		//a.setText("Stage1 : "+ bv.getTime() + " 초");
-		//b.setText("Stage2 : "+ bv.getTime() + " 초"); 
-		//c.setText("Stage3 : "+ bv.getTime() + " 초");
-		
+		bb.setContentAreaFilled(false);
+		bb.setFocusPainted(false);
+		bb.setIcon(new ImageIcon(Main.class.getResource("../image/check/backButton.png")));
+		bb.setSize(310, 160);
+
 	}
-	
-			
+
+	public void winCreator() {
+
+		sb.setBorderPainted(false);
+		sb.setContentAreaFilled(false);
+		sb.setFocusPainted(false);
+		sb.setIcon(new ImageIcon(Main.class.getResource("../image/check/startButton.png")));
+		sb.setSize(310, 160);
+		sb.addActionListener(this);
+		this.pb = new PlayerButton(p, null);
+
+		this.eb = new EnemyButton(e);
+		eb.setEnabled(false);
+
+		wb.setBorderPainted(false);
+		wb.setContentAreaFilled(false);
+		wb.setFocusPainted(false);
+		wb.setIcon(new ImageIcon(Main.class.getResource("../image/check/Winicon.png")));
+		wb.setSize(240, 140);
+
+		lb.setBorderPainted(false);
+		lb.setContentAreaFilled(false);
+		lb.setFocusPainted(false);
+		lb.setIcon(new ImageIcon(Main.class.getResource("../image/check/Loseicon.png")));
+		lb.setSize(240, 140);
+
+	}
+
+	public void finalCreator() {
+
+		this.pb = new PlayerButton(p, null);
+
+		wb.setBorderPainted(false);
+		wb.setContentAreaFilled(false);
+		wb.setFocusPainted(false);
+		wb.setIcon(new ImageIcon(Main.class.getResource("../image/check/Winicon.png")));
+		wb.setSize(240, 140);
+
+		xb.setBorderPainted(false);
+		xb.setContentAreaFilled(false);
+		xb.setFocusPainted(false);
+		xb.setIcon(new ImageIcon(Main.class.getResource("../image/check/exitButton.png")));
+		xb.setSize(310, 160);
+		bb.setBorderPainted(false);
+		bb.setContentAreaFilled(false);
+		bb.setFocusPainted(false);
+		bb.setIcon(new ImageIcon(Main.class.getResource("../image/check/backButton.png")));
+//	    bb.setIcon(new ImageIcon(Main.class.getResource("../image/check/????.png")));
+		bb.setSize(310, 160);
+		// this.a = new JLabel();
+		// a.setText("Stage1 : "+ bv.getTime() + " 초");
+		// b.setText("Stage2 : "+ bv.getTime() + " 초");
+		// c.setText("Stage3 : "+ bv.getTime() + " 초");
+
+	}
+
 	public void defaultViewSet() {
-		selectBackground = new ImageIcon(Main.class.getResource("../image/background/stage"+ stageNum +".jpg")).getImage();
+		selectBackground = new ImageIcon(Main.class.getResource("../image/background/stage" + stageNum + ".jpg"))
+				.getImage();
 		setUndecorated(true);
 		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
 		this.setResizable(false);
@@ -209,6 +206,7 @@ public class CheckView extends JFrame implements ActionListener{
 		this.setLayout(null);
 
 	}
+
 	public void paint(Graphics g) {
 		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
 		screenGraphic = screenImage.getGraphics();
@@ -221,74 +219,85 @@ public class CheckView extends JFrame implements ActionListener{
 		paintComponents(g);
 		this.repaint();
 	}
+
 	public void cpnsAdd() {
-		for(int i =0; i<this.cpns.length; i++)
+		for (int i = 0; i < this.cpns.length; i++)
 			add(this.cpns[i]);
 	}
-	
-	public void cpnsSetLocation() { 
+
+	public void cpnsSetLocation() {
 		wb.setLocation(120, 80);
-		lb.setLocation(870, 100 );
-		sb.setLocation(490 ,500);
-		vb.setLocation(550 ,400);
+		lb.setLocation(870, 100);
+		sb.setLocation(490, 500);
+		vb.setLocation(550, 400);
 		pb.setLocation(40, 80);
 		eb.setLocation(790, 110);
 		rb.setLocation(490, 500);
 		bb.setLocation(490, 400);
-		
+
 	}
-	
+
 	public void winSetLocation() {
 		this.wb.setLocation(120, 80);
-		this.lb.setLocation(870, 100 );
-		this.sb.setLocation(490 ,500);
+		this.lb.setLocation(870, 100);
+		this.sb.setLocation(490, 500);
 		this.pb.setLocation(40, 80);
 		this.eb.setLocation(790, 110);
-		
+
 	}
+
 	public void finalSetLocation() {
 		this.wb.setLocation(120, 80);
 		this.pb.setLocation(40, 80);
-		this.rb.setLocation(870, 500);
+		this.xb.setLocation(870, 500);
 		this.bb.setLocation(870, 400);
 	}
+
 	public void loseSetLocation() {
 		this.wb.setLocation(870, 80);
-		this.lb.setLocation(120, 90 );
+		this.lb.setLocation(120, 90);
 		this.rb.setLocation(490, 500);
 		this.bb.setLocation(490, 400);
 		this.pb.setLocation(40, 80);
 		this.eb.setLocation(790, 110);
 	}
 
-	public void buttonAddActionListener()
-	{
-		sb.addActionListener(new ActionListener() 
-		{
+	public void buttonAddActionListener() {
+		sb.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
 			}
 		});
-		rb.addActionListener(new ActionListener() 
-		{
+		rb.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				restart = false;
 				dispose();
 			}
 		});
-		bb.addActionListener(new ActionListener() 
-		{
+		bb.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				restart = true;
 				dispose();
 			}
+
 		});
+		xb.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+
+		});
+		
 	}
-	public boolean restart() {return this.restart;}
+
+	public boolean restart() {
+		return this.restart;
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		dispose();
-		
+
 	}
-	
-}	
+
+}
